3ff66742265c8d2f3ed18d7a53f9ebb68c8daaf2
Z3key <galaxy.s5.n950a@gmail.com>
2026-02-21 15:33:10 +0200

    start-project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import ThreeScene from './components/ThreeScene'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1>Hello, React3D</h1>
+        <ThreeScene />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ThreeScene.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
+import { OrbitControls } from '@react-three/drei';
+
+
+function Box() {
+    return (
+        <mesh rotation={[0.4, 0.2, 0]}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial />
+        </mesh>
+    )
+}
+
+export default function ThreeScene() {
+    return (
+        <Canvas style={{height: 400}}>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box />
+	        <primitive object={new THREE.AxesHelper(2)} />
+	        <primitive object={new THREE.GridHelper(10, 10)} />
+            <OrbitControls />
+        </Canvas>
+    )
+}
